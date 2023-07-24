38237a39bfe119dd169c062a31b7da67d7640f83
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-24 14:58:06 +0300

    css-finished

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-     Hi!
+    <div className='App'>
+      <h1 className="header">
+        British Meals
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/2560px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png"
+          alt="meals"
+        />
+      </h1>
+
+      <div className="container">
+      <div className="card">
+        <div className="image">
+          <img src="" alt="" />
+        </div>
+      </div>
+        <button>Click for More Food</button>
+      </div>
+
     </div>
   );
 }
