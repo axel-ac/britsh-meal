cf7a640c5699d2b7e30e55988f7dd64bc5a7b1c9
axel-ac <abdullahceylan.axel@gmail.com>
2023-07-24 17:11:59 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+const [item, setItem] = useState([])
+const [visible, setVisible] = useState(3)
+
+function showMoreFood(){
+  setVisible(a => a+=3)
+}
+
+useEffect(() => {
+  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?a=british")
+  .then((res) => res.json())
+  .then((data) => setItem(data.meals))
+}, [])
+
   return (
-    <div className='App'>
+    <div className="App">
       <h1 className="header">
         British Meals
         <img
@@ -12,14 +27,17 @@ function App() {
       </h1>
 
       <div className="container">
-      <div className="card">
-        <div className="image">
-          <img src="" alt="" />
-        </div>
-      </div>
-        <button>Click for More Food</button>
-      </div>
+        {item.slice(0, visible).map((item) => (
+          <div className="card">
+            <div className="image">
+              <img src={item.strMealThumb} alt="" />
+            </div>
+            <p>{item.strMeal}</p>
+          </div>
+        ))}
 
+        <button onClick={showMoreFood}>Click for More Food</button>
+      </div>
     </div>
   );
 }
